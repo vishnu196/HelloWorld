b258bb86f11164df267131608010e83425990570
vishnu196 <34365900+vishnu196@users.noreply.github.com>
2023-01-13 06:45:21 +0530

    DEV-1-Shell-Script (#2)
    * Hello world scripted to print hello world text in different colors..
    
    * This script updated with xtra colors
    
    * This script updated with more xtra colors
    
    Co-authored-by: vishnu <vishnu.king09@gmail.com>

---
+++ helloworld.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -e "\e[31m Hello World \e[0m"
+echo -e "\e[32m Hello World \e[0m"
+echo -e "\e[33m Hello World \e[0m"
+echo -e "\e[34m Hello World \e[0m"
+echo -e "\e[35m Hello World \e[0m"
+
